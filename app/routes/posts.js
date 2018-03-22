3f5272311f9fe474f8af47004f1c85fb28a46986
Josh Sorensen <joshua.sorensen9@gmail.com>
2018-03-21 19:11:57 -0600

    first commit

---
+++ app/routes/posts.js
@@ -0,0 +1,9 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+    model() {
+        $.getJSON("http://159.89.148.4:8081/posts", (err, data) => {
+            return data;
+        });
+    }
+});
