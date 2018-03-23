b38c56a83de95ea94213551d4cea916a5d501ef6
Josh Sorensen <joshua.sorensen9@gmail.com>
2018-03-22 23:00:27 -0600

    fix query route 5

--- app/routes/posts/edit.js
+++ app/routes/posts/edit.js
@@ -7,7 +7,7 @@ export default Route.extend({
     },
     id: null,
     ajax: Ember.inject.service(),
-    post(params) {
+    model(params) {
         var param = this.store.query('edit', params);
         return this.get('ajax').request("http://159.89.148.4:8081/posts/" + id, {
             method: 'GET'
