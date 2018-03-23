fd9c5752801fc68eea71328dd007d41eb58149e3
Josh Sorensen <joshua.sorensen9@gmail.com>
2018-03-22 22:51:36 -0600

    fix query route 3

--- app/routes/posts/details.js
+++ app/routes/posts/details.js
@@ -2,10 +2,13 @@ import Route from '@ember/routing/route';
 import Ember from 'ember';
 
 export default Route.extend({
-    queryParams: ['id'],
+    beforeModel: function(transition) {
+        this.id = transition.queryParams.id;
+    },
     id: null,
     ajax: Ember.inject.service(),
     post(params) {
+        var id = this.get('store')
         return this.get('ajax').request("http://159.89.148.4:8081/posts/" + id, {
             method: 'GET'
         });
