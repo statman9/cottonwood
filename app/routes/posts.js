02eb2b9160825cd21f2b190207a9cf5b5cf05fd7
Josh Sorensen <joshua.sorensen9@gmail.com>
2018-03-22 18:31:59 -0600

    ajax call

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,9 +1,10 @@
 import Route from '@ember/routing/route';
 import $ from 'jquery';
+import Ember from 'ember';
 
 export default Route.extend({
     model() {
-        $.getJSON("http://159.89.148.4:8081/posts", (err, data) => {
+        Ember.$.getJSON("http://159.89.148.4:8081/posts", (err, data) => {
             return data;
         });
     }
