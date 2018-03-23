4624b0cd6e40fc653b22b382295499c5d9bee777
Josh Sorensen <joshua.sorensen9@gmail.com>
2018-03-22 21:07:45 -0600

    fix query route

--- app/routes/posts/details.js
+++ app/routes/posts/details.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 
 export default Route.extend({
     queryParams: ['id'],
+    id: null,
     ajax: Ember.inject.service(),
     post(params) {
-        var param = this.store.query('details', params);
-        return this.get('ajax').request("http://159.89.148.4:8081/posts/" + param.id, {
+        return this.get('ajax').request("http://159.89.148.4:8081/posts/" + id, {
             method: 'GET'
         });
     }
--- app/routes/posts/edit.js
+++ app/routes/posts/edit.js
@@ -3,10 +3,11 @@ import Ember from 'ember';
 
 export default Route.extend({
     queryParams: ['id'],
+    id: null,
     ajax: Ember.inject.service(),
     post(params) {
         var param = this.store.query('edit', params);
-        return this.get('ajax').request("http://159.89.148.4:8081/posts/" + param.id, {
+        return this.get('ajax').request("http://159.89.148.4:8081/posts/" + id, {
             method: 'GET'
         });
     }
