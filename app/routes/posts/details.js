d164bc0efc9a38f9246b850f548725c7dca1a29b
Josh Sorensen <joshua.sorensen9@gmail.com>
2018-03-26 12:38:32 -0600

    details section template

--- app/routes/posts/details.js
+++ app/routes/posts/details.js
@@ -4,8 +4,7 @@ import Ember from 'ember';
 export default Route.extend({
     ajax: Ember.inject.service(),
     beforeModel: function(transition) {
-        this.set('id', transition.queryParams.id);
-        this.set('post', this.get('ajax').request("http://159.89.148.4:8081/posts/" + id, {
+        this.set('post', this.get('ajax').request("http://159.89.148.4:8081/posts/" + transition.queryParams.id, {
             method: "GET"
         })[0]);
     }
