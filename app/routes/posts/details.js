9e2b776d0009f3e655933e3a8785ac421bf62b6c
Josh Sorensen <joshua.sorensen9@gmail.com>
2018-03-26 11:19:18 -0600

    details section

--- app/routes/posts/details.js
+++ app/routes/posts/details.js
@@ -2,13 +2,11 @@ import Route from '@ember/routing/route';
 import Ember from 'ember';
 
 export default Route.extend({
+    ajax: Ember.inject.service(),
     beforeModel: function(transition) {
         this.set('id', transition.queryParams.id);
-    },
-    ajax: Ember.inject.service(),
-    model() {
-        return this.get('ajax').request("http://159.89.148.4:8081/posts/" + id, {
-            method: 'GET'
-        })[0];
+        this.set('post', this.get('ajax').request("http://159.89.148.4:8081/posts/" + id, {
+            method: "GET"
+        })[0]);
     }
 });
