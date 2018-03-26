01223b1b716f80b5956d882b3061c40e0ca2ec72
Josh Sorensen <joshua.sorensen9@gmail.com>
2018-03-26 11:12:43 -0600

    details section

--- app/routes/posts/details.js
+++ app/routes/posts/details.js
@@ -3,12 +3,10 @@ import Ember from 'ember';
 
 export default Route.extend({
     beforeModel: function(transition) {
-        this.id = transition.queryParams.id;
+        this.set('id', transition.queryParams.id);
     },
-    id: null,
     ajax: Ember.inject.service(),
-    post(params) {
-        var id = this.get('store')
+    model() {
         return this.get('ajax').request("http://159.89.148.4:8081/posts/" + id, {
             method: 'GET'
         });
