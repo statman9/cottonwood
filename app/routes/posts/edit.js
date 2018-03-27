0ec8c3b619a1c36e3641eac9de2ed9f15cb0dba0
Josh Sorensen <joshua.sorensen9@gmail.com>
2018-03-26 18:12:52 -0600

    form changes

--- app/routes/posts/edit.js
+++ app/routes/posts/edit.js
@@ -2,14 +2,10 @@ import Route from '@ember/routing/route';
 import Ember from 'ember';
 
 export default Route.extend({
-    beforeModel: function(transition) {
-        this.id = transition.queryParams.id;
-    },
-    id: null,
     ajax: Ember.inject.service(),
-    model() {
-        return this.get('ajax').request("http://159.89.148.4:8081/posts/" + this.id, {
+    beforeModel: function(transition) {
+        this.set('post', this.get('ajax').request("http://159.89.148.4:8081/posts/" + transition.queryParams.id, {
             method: 'GET'
-        });
-    }
+        })[0]);
+    },
 });
