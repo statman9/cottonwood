d360634880d1cb230252c81ece50825d7285f77c
Josh Sorensen <joshua.sorensen9@gmail.com>
2018-03-26 18:29:31 -0600

    change api call

--- app/routes/posts/edit.js
+++ app/routes/posts/edit.js
@@ -2,10 +2,9 @@ import Route from '@ember/routing/route';
 import Ember from 'ember';
 
 export default Route.extend({
-    ajax: Ember.inject.service(),
     beforeModel: function(transition) {
-        this.set('post', this.get('ajax').request("http://159.89.148.4:8081/posts/" + transition.queryParams.id, {
-            method: 'GET'
-        })[0]);
-    },
+        $.getJSON("http://159.89.148.4:8081/posts/" + transition.queryParams.id).then(data => {
+          this.set('posts', data[0]);
+        });
+    }
 });
