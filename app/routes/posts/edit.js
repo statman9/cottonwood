88a68b01749857c433365adf57b8ff51e073ce71
Josh Sorensen <joshua.sorensen9@gmail.com>
2018-03-26 18:31:46 -0600

    fix set

--- app/routes/posts/edit.js
+++ app/routes/posts/edit.js
@@ -4,7 +4,7 @@ import Ember from 'ember';
 export default Route.extend({
     beforeModel: function(transition) {
         $.getJSON("http://159.89.148.4:8081/posts/" + transition.queryParams.id).then(data => {
-          this.set('posts', data[0]);
+          this.set('post', data[0]);
         });
     }
 });
